537e4a971ddb21ab1ceccfb43073342bcbd715fe
CJ Carey <perimosocordiae@gmail.com>
2015-01-09 10:33:06 -0500

    Simplifying missing_lines, fixes #1

--- scripts/missing_lines.py
+++ scripts/missing_lines.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-import os
 import sys
 from coverage import coverage
 from coverage.files import FnmatchMatcher
@@ -9,12 +8,9 @@ def missing_lines(cov_file, cov_config, fname):
   cov = coverage(data_file=cov_file, config_file=cov_config)
   if FnmatchMatcher(cov.omit).match(fname):
     return
-  cov_dir = os.path.dirname(cov_file)
-  relpath = os.path.relpath(fname, cov_dir)
   cov.load()
-  missing_lines = cov.analysis(relpath)[2]
-  for n in missing_lines:
-    print(n)
+  missing_lines = cov.analysis(fname)[2]
+  print(*missing_lines, sep='\n', end='')
 
 if __name__ == '__main__':
   missing_lines(*sys.argv[1:])
