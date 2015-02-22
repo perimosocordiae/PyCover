761ebb40cc3a06a87aafccc63167abbfbb90fe49
CJ Carey <perimosocordiae@gmail.com>
2015-02-22 18:17:35 -0500

    remove unnecessary code from missing_lines.py
    Should hopefully solve issue #3.

--- scripts/missing_lines.py
+++ scripts/missing_lines.py
@@ -1,13 +1,10 @@
 from __future__ import print_function
 import sys
 from coverage import coverage
-from coverage.files import FnmatchMatcher
 
 
 def missing_lines(cov_file, cov_config, fname):
   cov = coverage(data_file=cov_file, config_file=cov_config)
-  if FnmatchMatcher(cov.omit).match(fname):
-    return
   cov.load()
   missing_lines = cov.analysis(fname)[2]
   print(*missing_lines, sep='\n', end='')
