85edbb2533484d364e37606dc88b842e54dd2d80
CJ Carey <perimosocordiae@gmail.com>
2017-08-13 17:18:52 -0400

    Prevent showing omitted files as all missing
    Fixes gh-7.

--- scripts/missing_lines.py
+++ scripts/missing_lines.py
@@ -6,6 +6,11 @@ from coverage import coverage
 def missing_lines(cov_file, cov_config, fname):
   cov = coverage(data_file=cov_file, config_file=cov_config)
   cov.load()
+  cov.start()
+  if cov.omit_match and cov.omit_match.match(fname):
+    sys.stderr.write('Omitted file.\n')
+    sys.exit(1)
+
   missing_lines = cov.analysis(fname)[2]
   print(*missing_lines, sep='\n', end='')
 
